5c6ca0dbd300de261611ffdf538f2c0addb57c60
Elbert Jean <elbertjeandossantos@gmail.com>
2025-03-11 23:27:34 -0300

    feat (#4): configuracao de todo fluxo do template

---
+++ src/components/NavBar/NavBar.vue
@@ -0,0 +1,37 @@
+<script>
+import {RouterLink} from 'vue-router'
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark navbar-custom" style="height: 100px;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img alt="Morpheus Logo" src="../../assets/Logo_Morpheus.png" width="auto" height="60">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-4">
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/mapa">Mapa</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/dashboard">Dashboard</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/configuracao">Configuração</RouterLink>
+                    </li>
+                    <li class="nav-item">
+                        <RouterLink class="nav-link" to="/">Sair</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
---
+++ src/view/LoginView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <div class="d-flex flex-column justify-content-center align-items-center vh-100 gap-4">
+        <h1 class="h1 text-center mt-5">Tela Login</h1>
+        <button type="button" class="btn btn-primary btn-lg w-25">
+            <RouterLink class="nav-link text-white" to="/mapa">Login</RouterLink>
+        </button>
+    </div>
+</template>
