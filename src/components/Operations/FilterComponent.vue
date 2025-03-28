0351981c591e9228efbbe28d2bea87672447a565
gabriel15asouza <gabalves.souza@gmail.com>
2025-03-28 13:06:52 -0300

    feat: inicializando components table e filter

---
+++ src/components/Operations/FilterComponent.vue
@@ -0,0 +1,118 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const emit = defineEmits(['apply-filters']);
+
+const filters = ref({
+  fazenda: '',
+  cidade: '',
+  estado: '',
+  situacao: '',
+});
+
+const options = ['Opção 1', 'Opção 2', 'Opção 3'];
+
+const applyFilters = () => {
+  emit('apply-filters', { ...filters.value });
+};
+</script>
+
+<template>
+  <div class="filter-component py-4 px-4 bg-white shadow">
+    <!-- Botão Criar Talhão -->
+     <div class="d-flex w-100 gap-2 justify-content-around align-items-center">
+       <h1 class="h1 display-5 fw-bold font-weight-bold font-size">Operações</h1>
+        <div class="btn-component d-flex flex-column justify-content-end align-items-start align-items-center h-100">
+          <button class="btn-criar btn btn-primary ">Criar Talhão</button>
+        </div>
+
+        <div class="filters-container d-flex">
+          <!-- Filtro Fazenda -->
+          <div class="filter-item">
+            <label for="fazendaFilter" class="form-label">Fazenda</label>
+            <select id="fazendaFilter" v-model="filters.fazenda" class="form-select">
+              <option value="">Selecione a fazenda...</option>
+              <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+
+          <!-- Filtro Cidade -->
+          <div class="filter-item">
+            <label for="cidadeFilter" class="form-label">Cidade</label>
+            <select id="cidadeFilter" v-model="filters.cidade" class="form-select">
+              <option value="">Selecione a cidade...</option>
+              <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+
+          <!-- Filtro Estado -->
+          <div class="filter-item">
+            <label for="estadoFilter" class="form-label">Estado</label>
+            <select id="estadoFilter" v-model="filters.estado" class="form-select">
+              <option value="">Selecione o estado...</option>
+              <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+
+          <!-- Filtro Situação -->
+          <div class="filter-item">
+            <label for="situacaoFilter" class="form-label">Situação</label>
+            <select id="situacaoFilter" v-model="filters.situacao" class="form-select">
+              <option value="">Selecione a situação...</option>
+              <option v-for="option in options" :key="option" :value="option">
+                {{ option }}
+              </option>
+            </select>
+          </div>
+          <div class="filter-item">
+            <button @click="applyFilters" class="btn btn-primary">Aplicar filtros</button>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.filter-component {
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding-bottom: 2px;
+  justify-content: spa;
+}
+
+.btn-component h1 {
+  font-weight: bold;
+  font-size: 1.5rem;
+  padding-bottom: 10px;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  color: #333;
+  letter-spacing: 0.2px;
+}
+
+.btn-criar {
+  padding: 1rem;
+  width: 180px;
+}
+
+.filters-container {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+
+.filter-item {
+  flex: 1 1 200px;
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/components/Operations/TableComponent.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import areasSJC from '../Map/data/areasSJC';
+import areasMock from '../Map/data/areasMock.json'
+import FilterComponent from '@/components/Operations/FilterComponent.vue';
+
+const router = useRouter();
+
+const handlePrintId = (id) => {
+  if (!areasSJC || !Array.isArray(areasSJC.features)) {
+    console.error("O objeto 'areasSJC' não está carregado corretamente");
+    return;
+  }
+
+  const area = areasSJC.features.find(area => area.properties.id === id);
+
+  if (area) {
+    router.push({
+      name: 'operacaoMapDetails',
+      params: { id: area.properties.id },
+      state: { operation: area }
+    });
+  } else {
+    console.log(`Área com id ${id} não encontrada.`);
+  }
+  console.log(areasMock);
+};
+</script>
+
+<template>
+  <div class="w-100 h-100 d-flex flex-column gap-5 rounded bg-red pt-5">
+    <FilterComponent />
+    <div class="table-container flex-grow-1 overflow-auto rounded">
+      <table class="table table-striped table-bordered">
+        <thead class="sticky-top">
+          <tr>
+            <th class="col text-center bg-dark text-white p-4">Nome</th>
+            <th class="col text-center bg-dark text-white p-4">Descrição</th>
+            <th class="col text-center bg-dark text-white p-4">Fazenda</th>
+            <th class="col text-center bg-dark text-white p-4">Cidade/Estado</th>
+            <th class="col text-center bg-dark text-white p-4">Situação</th>
+            <th class="col text-center bg-dark text-white p-4" style="min-width: 200px">Ação</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="area in areasSJC.features" :key="area.properties.id">
+            <td class="text-center px-3 py-3">{{ area.properties.alt }}</td>
+            <td class="text-wrap py-3">{{ area.properties.description }}</td>
+            <td class="text-center py-3">{{ area.properties.fazenda ? area.properties.fazenda.nome : 'N/A' }}</td>
+            <td class="text-center py-3">{{ area.properties.fazenda.cidade + ' - ' + area.properties.fazenda.estado }}</td>
+            <td class="text-center py-3">Pendente</td>
+            <td class="text-center px-3">
+              <button @click="handlePrintId(area.properties.id)" class="btn btn-primary">Ver operação</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.table-container {
+  max-height: calc(100vh - 140px);
+}
+
+.sticky-top {
+  position: sticky;
+  top: 0;
+  z-index: 1020;
+}
+
+.text-wrap {
+  word-wrap: break-word;
+  white-space: normal;
+}
+</style>
