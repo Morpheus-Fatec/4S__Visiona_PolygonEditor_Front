4c725b779e1b9d94b81ce9d0d2c42395a13a8b0a
gabriel15asouza <gabalves.souza@gmail.com>
2025-04-18 12:06:43 -0300

    Style(#32):novo style filtros para incluir paginas

--- src/components/Operations/FilterComponent.vue
+++ src/components/Operations/FilterComponent.vue
@@ -23,7 +23,7 @@ const applyFilter = () => {
 
 <template>
   <div class="filter-component bg-white shadow">
-    <div class="filters-container h-100 p-4">
+<div class="filters-container h-100 pt-4 pe-4 ps-4 pb-0">
       <div class="filter-item">
         <label for="talhaoFilter" class="h6">Nome do talhão</label>
         <input type="text" id="input-talhao" class="form-control" v-model="filters.name" maxlength="8" size="10" placeholder="Insira talhão..."/>
@@ -50,7 +50,7 @@ const applyFilter = () => {
       </div>
     </div>
 
-    <div class="w-100 d-flex justify-content-center mt-3">
+    <div class="w-100 d-flex justify-content-center">
       <button @click="applyFilter" class="btn btn-primary w-100">Aplicar filtros</button>
     </div>
   </div>
@@ -64,7 +64,6 @@ const applyFilter = () => {
   flex-direction: column;
   width: 400px;
   gap: 1rem;
-  padding-bottom: 2px;
 }
 
 .btn-component h1 {
@@ -77,20 +76,15 @@ const applyFilter = () => {
   letter-spacing: 0.2px;
 }
 
-.btn-criar {
-  padding: 1rem;
-  width: 180px;
-}
-
 .filters-container {
   display: flex;
   flex-wrap: wrap;
+  gap: 8px;
 }
 
 .filter-item {
   flex: 1 1 200px;
   display: flex;
   flex-direction: column;
-  margin-top: 15px;
 }
 </style>
