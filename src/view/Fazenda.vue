7401149456a70309270ee590b2cc87f844485008
Ricardo Campos <70415796+r1cardvs@users.noreply.github.com>
2025-04-22 22:22:08 -0300

    feat (#35) Incluir gerenciamento de Fazendas

---
+++ src/view/Fazenda.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Layout from '@/components/Layout/Layout.vue';
+import Farm from '@/components/Farm/Mananger.vue';
+
+</script>
+
+<template>
+    <Layout>
+        <div class="background">
+          <Farm/>
+        </div>
+    </Layout>
+</template>
+
+<style scoped>
+.background {
+  background-color: #FFFFFF;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+}
+</style>
