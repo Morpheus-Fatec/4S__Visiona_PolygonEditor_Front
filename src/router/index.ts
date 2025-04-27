21ce2bc0ad9ae0c970fd42937f182f578660c0bc
gabriel15asouza <gabalves.souza@gmail.com>
2025-04-27 13:44:21 -0300

    feat:(42): autentica token para realizar login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../view/DashboardView.vue';
 import Configuracao from '../view/ConfiguracaoView.vue';
 import Login from '../view/LoginView.vue';
@@ -10,6 +10,11 @@ import Cultura from '@/view/Cultura.vue';
 import Solo from '@/view/Solo.vue';
 import Fazenda from '@/view/Fazenda.vue';
 import UserRegisterView from '@/view/UserRegisterView.vue';
+
+function isAuthenticated() {
+  const token = localStorage.getItem("token");
+  return token !== null; 
+};
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,59 +28,78 @@ const router = createRouter({
       path: '/mapa-global',
       name: 'mapa-global',
       component: () => import ('../view/MapaGlobalView.vue'),
+      meta: { requiresAuth: true } 
     },
     {
       path: '/operacao/:id',
       name: 'operacaoMapDetails',
       props: true,
       component: MapaOperationView,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/dashboard',
       name: 'dashboard',
       component: Dashboard,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/configuracao',
       name: 'configuracao',
       component: Configuracao,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/operacoes',
       name: 'operacoes',
       component: Operacoes,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/',
       name: 'register',
       component: RegisterView,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
       component: NotFound,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/cultura',
       name:'cultura',
-      component: Cultura
+      component: Cultura,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/solo',
       name:'solo',
-      component: Solo
+      component: Solo,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/fazenda',
       name:'fazenda',
-      component: Fazenda
+      component: Fazenda,
+      meta: { requiresAuth: true } 
     },
     {
       path: '/usuario',
       name: 'usuario',
       component: UserRegisterView,       
+      meta: { requiresAuth: true } 
     }
   ]
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    next('/'); 
+  } else {
+    next();
+  }
+});
+
+export default router;
